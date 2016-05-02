ffe5933e595041577eaf369b611b3e0fe190e018
Hardik <hardik.webatease@gmail.com>
2016-05-03 02:48:02 +0530

    - Carthage support

---
+++ AFDateHelper-lib/AFDateHelper-lib.h
@@ -0,0 +1,19 @@
+//
+//  AFDateHelper-lib.h
+//  AFDateHelper-lib
+//
+//  Created by Hardik on 5/3/16.
+//  Copyright © 2016 All Forces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFDateHelper-lib.
+FOUNDATION_EXPORT double AFDateHelper_libVersionNumber;
+
+//! Project version string for AFDateHelper-lib.
+FOUNDATION_EXPORT const unsigned char AFDateHelper_libVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFDateHelper_lib/PublicHeader.h>
+
+
